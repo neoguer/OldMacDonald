897d4e6be02e575a975f2d60a5fab2577461ca53
NEO GUERRERO <NEGUERRERO@sfusd.edu>
2018-11-02 10:00:30 -0700

    New

--- Chick.java
+++ Chick.java
@@ -1,4 +1,26 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal {     
+  private String myType;     
+  private String mySound;     
+  private String mySound2;
+  public Chick(String type, String sound, String sound2) {         
+    myType = type;         
+    mySound = sound;
+    mySound2 = sound2;
+  }     
+  public Chick() {         
+    myType = "unknown";         
+    mySound = "unknown";
+    mySound2 = "unkown";
+  }      
+  public String getSound() {
+    if (Math.random() < 0.5)
+    
+    return mySound;
+    
+    else 
+    return mySound2;
+  }     
+  public String getType() {
+    return myType;
+  }
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,21 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+  private String myName;
+  
+  public NamedCow(String type, String sound, String name)
+  {
+    myType = type;
+    mySound = sound;
+    myName = name;
+  }
+  public NamedCow()
+  {
+    myType = "unknown";
+    mySound = "unknown";
+    myName = "unknown";
+  }
+  public String getName()
+  {
+    return myName;
+  }
 }
